d30acecc7e265119844d6a129ac7293288436edf
Ben Kannenberg <bkannenberg@gmail.com>
2016-12-22 14:12:37 +1030

    Add sass watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,26 @@
+var ant = require('postcss-ant');
 var autoprefixer = require('autoprefixer')
 var cssnano = require('cssnano')
 var cssnext = require('cssnext')
-var gulp = require('gulp')
+var gulp = require('gulp');
 var mqpacker = require('css-mqpacker')
-var postcss = require('gulp-postcss')
-var rucksack = require('rucksack')
+var postcss = require('gulp-postcss');
+var sass = require('gulp-sass');
+
+// var rucksack = require('rucksack')
+
+gulp.task('styles', function() {
+    var processors = [
+        ant,
+        autoprefixer({browsers:['last 2 versions']})
+    ]
+
+    return gulp.src('./src/stylesheets/main.scss')
+        .pipe(sass())
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('watch:styles', function() {
+    gulp.watch('./src/stylesheets/**/*.scss', ['styles']);
+});
