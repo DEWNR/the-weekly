0643d8ff7f1dd5814b242fcb313340d6e187d32a
Ben Kannenberg <bkannenberg@gmail.com>
2016-12-28 16:39:34 +1030

    Basic grid set up

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,50 @@
 var ant = require('postcss-ant');
-var autoprefixer = require('autoprefixer')
-var cssnano = require('cssnano')
-var cssnext = require('cssnext')
+var autoprefixer = require('autoprefixer');
+var browserSync = require('browser-sync').create();
+var cssnano = require('cssnano');
+var cssnext = require('cssnext');
 var gulp = require('gulp');
-var mqpacker = require('css-mqpacker')
+var mqpacker = require('css-mqpacker');
 var postcss = require('gulp-postcss');
+var rucksack = require('rucksack')
 var sass = require('gulp-sass');
 
-// var rucksack = require('rucksack')
+var src = {
+    scss: 'src/stylesheets/**/*.scss',
+    images: 'src/images/**/*.png',
+    html: 'src/html/**/*.html'
+};
 
+// Static Server + watching scss/html files
+gulp.task('serve', ['styles'], function() {
+
+    browserSync.init({
+        server: "./dist"
+    });
+
+    gulp.watch(src.scss, ['styles']);
+    gulp.watch(src.html, ['templates']);
+    gulp.watch(src.images, ['images']);
+});
+
+
+// Templates
+gulp.task('templates', function() {
+    return gulp.src(src.html)
+        .pipe(gulp.dest('./dist'))
+        .pipe(browserSync.stream());
+});
+
+
+// Images
+gulp.task('images', function() {
+    return gulp.src(src.images)
+        .pipe(gulp.dest('./dist/images'))
+        .pipe(browserSync.stream());
+});
+
+
+// Compile sass into CSS
 gulp.task('styles', function() {
     var processors = [
         ant,
@@ -18,9 +54,8 @@ gulp.task('styles', function() {
     return gulp.src('./src/stylesheets/main.scss')
         .pipe(sass())
         .pipe(postcss(processors))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('watch:styles', function() {
-    gulp.watch('./src/stylesheets/**/*.scss', ['styles']);
-});
+gulp.task('default', ['serve', 'templates', 'images']);
