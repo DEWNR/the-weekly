b68532d512847f82e85aa320db6857f09380eb4f
Ben Kannenberg <bkannenberg@gmail.com>
2017-01-18 14:18:55 +1030

    Prelaunch mad rush

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,3 +11,21 @@ slideLeftBtn.addEventListener('click', function(e) {
     e.preventDefault;
     slideLeft.open();
 });
+
+
+// Print
+$('[data-js=print]').on('click', function() { window.print(); return false; });
+
+
+// Email
+$('[data-js=email]').on('click', function() {
+
+    var mailSubject = encodeURI(thisPage.title);
+
+    var mailBody = encodeURI('I thought you might like this post: '+thisPage.url);
+
+    window.location.href = 'mailto:?subject='+mailSubject+'&body='+mailBody;
+
+    return false;
+
+});
