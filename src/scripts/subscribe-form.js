3fa0f6b684ca59648f2990896e97be2bd592c755
Ben Kannenberg <bkannenberg@gmail.com>
2017-01-18 15:59:08 +1030

    Add subscribe form

--- src/scripts/subscribe-form.js
+++ src/scripts/subscribe-form.js
@@ -1,30 +1,19 @@
-// $("#campaign-monitor input[type='submit']").click(function() {
-//
-//     var emailComplete = false, subscribeToComplete = false;
-//
-//     var emailRegExp = new RegExp(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i);  // RegExp from http://www.regular-expressions.info/email.html
-//
-//
-//     if (emailRegExp.test($("#fieldEmail").val())) {
-//
-//       emailComplete = true;
-//
-//       $(".js-email .seamless-form__error").addClass('seamless-form__error--inactive');
-//
-//     } else {
-//
-//       $(".js-email .seamless-form__error").removeClass('seamless-form__error--inactive');
-//
-//     }
-//
-//
-//     if (emailComplete) {
-//
-//       location.href = "http://dewnrcomms.createsend.com/t/r/s/ihlldhy/?"+$("#campaign-monitor input").serialize();
-//
-//     }
-//
-//
-//     return false;
-//
-// });
+$('.subscribe-form__submit').on( 'click', function() {
+
+    var emailComplete = false;
+    var subscribeToComplete = false;
+    var emailRegExp = new RegExp(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i);  // RegExp from http://www.regular-expressions.info/email.html
+
+    if (emailRegExp.test( $('#fieldEmail').val()) ) {
+        emailComplete = true;
+        $('.js-email div.subscribe-form__error').hide();
+    } else {
+        $('.js-email div.subscribe-form__error').show();
+    }
+
+    if (emailComplete) {
+        location = 'http://dewnrcomms.createsend.com/t/r/s/kudddhl/?' + $('.subscribe-form input').serialize();
+    }
+
+    return false;
+});
