24cb6fe48bdce66a1919ffa5d4610e72970f6077
Ben Kannenberg <bkannenberg@gmail.com>
2017-02-06 15:54:15 +1030

    Add descriptor to footer, add article caption styling

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -29,3 +29,43 @@ $('[data-js=email]').on('click', function() {
     return false;
 
 });
+
+
+var str = location.search;
+
+console.log(str.indexOf('People%20matter'))
+
+var categoryTitle = '';
+
+if (str.indexOf('Need%20to%20know') >= 0) {
+    categoryTitle = 'Need to know';
+    console.log('here');
+}
+
+if (str.indexOf('News') >= 0) {
+    categoryTitle = 'News';
+}
+
+if (str.indexOf('Social') >= 0) {
+    categoryTitle = 'Social';
+}
+
+if (str.indexOf('Community%20at%20the%20centre') >= 0) {
+    categoryTitle = 'Community at the centre';
+}
+
+if (str.indexOf('Profile') >= 0) {
+    categoryTitle = 'Profile';
+}
+
+if (str.indexOf('Events') >= 0) {
+    categoryTitle = 'Events';
+}
+
+if (str.indexOf('People%20matter') >= 0) {
+    categoryTitle = 'People matter';
+}
+
+if (categoryTitle) {
+    $('h1').append(' - ' + categoryTitle);
+}
