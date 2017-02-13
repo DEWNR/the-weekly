0638869d68cbee1ebdc16a05b85062b220c2ad5e
Ben Kannenberg <bkannenberg@gmail.com>
2017-02-13 12:43:27 +1030

    recreating package.json

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -31,41 +31,67 @@ $('[data-js=email]').on('click', function() {
 });
 
 
-var str = location.search;
+// var str = location.search;
+//
+// console.log(str.indexOf('People%20matter'))
+//
+// var categoryTitle = '';
+//
+// if (str.indexOf('Need%20to%20know') >= 0) {
+//     categoryTitle = 'Need to know';
+//     console.log('here');
+// }
+//
+// if (str.indexOf('News') >= 0) {
+//     categoryTitle = 'News';
+// }
+//
+// if (str.indexOf('Social') >= 0) {
+//     categoryTitle = 'Social';
+// }
+//
+// if (str.indexOf('Community%20at%20the%20centre') >= 0) {
+//     categoryTitle = 'Community at the centre';
+// }
+//
+// if (str.indexOf('Profile') >= 0) {
+//     categoryTitle = 'Profile';
+// }
+//
+// if (str.indexOf('Events') >= 0) {
+//     categoryTitle = 'Events';
+// }
+//
+// if (str.indexOf('People%20matter') >= 0) {
+//     categoryTitle = 'People matter';
+// }
+//
+// if (categoryTitle) {
+//     $('h1').append(' - ' + categoryTitle);
+// }
 
-console.log(str.indexOf('People%20matter'))
+// $('.card-teaser--blog').each(function(){
+//     // if ($(this).height() > maxHeight) { maxHeight = $(this).height(); }
+//
+//     // console.log($(this).height());
+//
+//     prevHeight = $(this).prev().height();
+//     thisHeight = $(this).height();
+//
+//     console.log(prevHeight)
+//     console.log(thisHeight)
+//
+//
+// });
 
-var categoryTitle = '';
 
-if (str.indexOf('Need%20to%20know') >= 0) {
-    categoryTitle = 'Need to know';
-    console.log('here');
-}
+var pairs = [];
+$('.card-teaser--blog').each(function(i, div) {
+    var i_over_2 = Math.floor(i / 2);
+    if (!pairs[i_over_2]) pairs[i_over_2] = $();
+    pairs[i_over_2] = pairs[i_over_2].add(div);
+});
 
-if (str.indexOf('News') >= 0) {
-    categoryTitle = 'News';
-}
-
-if (str.indexOf('Social') >= 0) {
-    categoryTitle = 'Social';
-}
-
-if (str.indexOf('Community%20at%20the%20centre') >= 0) {
-    categoryTitle = 'Community at the centre';
-}
-
-if (str.indexOf('Profile') >= 0) {
-    categoryTitle = 'Profile';
-}
-
-if (str.indexOf('Events') >= 0) {
-    categoryTitle = 'Events';
-}
-
-if (str.indexOf('People%20matter') >= 0) {
-    categoryTitle = 'People matter';
-}
-
-if (categoryTitle) {
-    $('h1').append(' - ' + categoryTitle);
-}
+$.each(pairs, function(i, p) {
+    console.log(p);
+});
